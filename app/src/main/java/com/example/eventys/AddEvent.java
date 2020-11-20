61a5ad23982c97d03e813e10aad7e429fffc00d3
AlexandruTabacaru <alexandru.tabacaru11@gmail.com>
2020-11-20 21:05:50 +0200

    saloot

--- app/src/main/java/com/example/eventys/AddEvent.java
+++ app/src/main/java/com/example/eventys/AddEvent.java
@@ -3,10 +3,10 @@ package com.example.eventys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//this is epic
+
 
 public class AddEvent extends AppCompatActivity {
-int a;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
